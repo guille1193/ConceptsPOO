72f6f40898fa4340d0c37b35e46ee75c8ab75b25
guille1193 <52618221+guille1193@users.noreply.github.com>
2022-05-14 22:40:37 -0400

    Finish Class Date

---
+++ ConceptsPOO/ConceptsPOO/Date.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptsPOO
+{
+    public class Date
+    {
+        private int _Year;
+        private int _Month;
+        private int _Day;
+
+        public Date(int year, int month, int day)
+        {
+            _Year = year;
+            _Month = CheckMonth(month);
+            _Day = CheckDay(year, month, day);
+        }
+
+        private int CheckDay(int year, int month, int day)
+        {
+            if(day == 2 && day == 29 && IsleapYear(year))
+            {
+                return day;
+            }
+            int[] daysPermonth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            if(day >=1 && day <= daysPermonth[month])
+            {
+                return day;
+            }
+            throw new DayException("Invalid Day");
+        }
+
+        private bool IsleapYear(int year)
+        {
+            return year % 400 == 0 || year % 4 == 0 && year % 100 !=0;
+        }      
+
+        private int CheckMonth(int month)
+        {
+            if(month>=0 && month<=12)
+            {
+                return month;
+
+            }
+            throw new MonthException("Invalid Month");
+        }
+
+        public override string ToString()
+        {
+            return $"{_Year}/{_Month:00}/{_Day:00}";
+        }
+
+    }
+}
---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+using ConceptsPOO;
+
+Console.WriteLine("CONCEPTS POO");
+Console.WriteLine("============");
+try
+{
+    Console.WriteLine(new Date(2022, 5, 1));
+    Console.WriteLine(new Date(2005, 12, 31));
+    Console.WriteLine(new Date(1970, 12, 14));
+}
+catch (Exception Error)
+{
+    Console.WriteLine(Error.Message);
+}
+
+
