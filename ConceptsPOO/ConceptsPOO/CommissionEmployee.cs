23a799495b4800ff97b920906c7ccdc49a9556bf
guille1193 <52618221+guille1193@users.noreply.github.com>
2022-05-20 23:20:45 -0400

    Employee, Salary Employee & commision employee

---
+++ ConceptsPOO/ConceptsPOO/CommissionEmployee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConceptsPOO
+{
+    public class CommissionEmployee : Employee
+    {
+        public float CommissionPercentaje { get; set; }
+
+        public decimal Sales { get; set; }   
+        public override decimal GetValueToPay()
+        {
+            return Sales * (decimal)CommissionPercentaje;
+        }
+
+        public override string ToString()
+        {
+            return $"{base.ToString()}" +
+                $"\n\tCommission Percentaje: {$"{CommissionPercentaje:p2}", 18} " +
+                $"\n\tSales................: {$"{Sales:c2}", 18} " +
+                $"\n\tvalue to pay:........: {$"{GetValueToPay():c2}",18} ";
+        }
+    }
+}
--- ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -6,15 +6,31 @@ using ConceptsPOO;
 
 Console.WriteLine("CONCEPTS POO");
 Console.WriteLine("============");
-try
+Employee employee1 = new SalaryEmployee()
 {
-    Console.WriteLine(new Date(2022, 5, 2));
-    Console.WriteLine(new Date(2005, 12, 31));
-    Console.WriteLine(new Date(1970, 12, 14));
-}
-catch (Exception Error)
+    Id = 2345,
+    FirtName = "Guillermo",
+    LastName = "Delgado",
+    BirthDate = new Date(1993, 11, 11),
+    HiringDate = new Date(2019, 03, 14),
+    IsActive = true,
+    Salary = 34025.15m
+};
+
+Console.WriteLine(employee1);
+Employee employee2 = new CommissionEmployee()
 {
-    Console.WriteLine(Error.Message);
-}
+    Id = 2346,
+    FirtName = "Yesenia",
+    LastName = "Contreras",
+    BirthDate = new Date(1998, 11, 10),
+    HiringDate = new Date(2020, 12, 14),
+    IsActive = true,
+    Sales = 25850m,
+    CommissionPercentaje=0.05f
+    
+};
+
+Console.WriteLine(employee2);
 
 
